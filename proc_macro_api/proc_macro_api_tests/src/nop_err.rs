649e1870419e27ab7285db21b51b1b9e78f8c490
boxcatgarden <30717198+BoxCatGarden@users.noreply.github.com>
2025-04-19 11:25:09 +0800

    proc_macro_api: test

--- proc_macro_api/proc_macro_api_tests/src/nop_err.rs
+++ proc_macro_api/proc_macro_api_tests/src/nop_err.rs
@@ -1,44 +1,71 @@
-proc_macro_api! {
-    // blk_al
-    {} as _,
+macro_rules! error {
+    ($($name:ident : { $($tt:tt)* }),* $(,)?) => {$(
+        /// ```
+        /// use ::proc_macro_api::proc_macro_api;
+        /// proc_macro_api! {
+        #[doc = concat!($(stringify!($tt), " ",)*)]
+        /// }
+        /// fn main() {}
+        /// ```
+        fn $name() {}
+    )*};
+}
 
-    // inner_cc
-    ::{::b as _},
-    a::{::b as _},
+error! {
+    blk_al: {
+        {} as _,
+    },
 
-    // no_seg
-    #[fn],
+    inner_cc_0: {
+        ::{::a::b as _},
+    },
+    inner_cc_1: {
+        a::{::b as _},
+    },
 
-    // mul_seg
-    a {},
-    a::a {},
-    a::a::a {},
-    a::a::a::{} {},
-    a::a::{} {},
-    a::{} {},
-    ::a {},
-    ::a::a {},
-    ::a::a::{} {},
-    ::a::{} {},
-    ::{} {},
+    no_seg: {
+        #[fn],
+    },
 
-    // no_proc
-    b,
-    ::a::b as b,
-    a::a::b,
+    mul_seg: {
+        a {},
+        a::a {},
+        a::a::a {},
+        a::a::a::{} {},
+        a::a::{} {},
+        a::{} {},
+        ::a {},
+        ::a::a {},
+        ::a::a::{} {},
+        ::a::{} {},
+        ::{} {},
+    },
 
-    // err_seg
-    {;},
-    {#[fn];},
-    {a;},
-    {::;},
-    {::a;},
-    {::a::a;},
-    {::a::a::;},
-    {::{};},
-    {{};},
-    {#[fn] b as ;},
+    no_proc_0: {
+        b,
+    },
+    no_proc_1: {
+        ::a::b as b,
+    },
+    no_proc_2: {
+        a::a::b,
+    },
+
+    err_seg_0: {
+        {;},
+        {#[fn];},
+        {a;},
+        {::;},
+        {::a;},
+        {::a::a;},
+        {::a::a::;},
+        {::{};},
+        {{};},
+    },
+    err_seg_1: {
+        {#[fn] b as ;},
+    },
 
     // no_path
-    #[fn] nonexistent_api,
+    // #[fn] nonexistent_api,
 }
