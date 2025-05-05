292f762e340d4da52e6ace9076ebff1547777d8a
boxcatgarden <30717198+BoxCatGarden@users.noreply.github.com>
2025-05-06 07:19:57 +0800

    test: attr

--- proc_macro_api_tests/src/err_attr/err_ap.rs
+++ proc_macro_api_tests/src/err_attr/err_ap.rs
@@ -5,7 +5,7 @@ error! {
     err_ap_3: { #[doc=""] {#[doc=""]#[doc=""]#[doc=""] a::a::b as _} },
     err_ap_4: { #[doc=""] {#[doc=""]#[doc=""] a::a::{}} },
     err_ap_5: { #[doc=""] {#[doc=""]#[doc=""]#[doc=""] a::a::a::{}} },
-    err_ap_6: { #[doc=""] {#[doc=""] a::a::b as _} },
-    err_ap_7: { #[doc=""] {#[doc=""] a::a::a::b as _} },
+    err_ap_6: { #[doc=""]#[doc=""] {#[doc=""] a::a::b as _} },
+    err_ap_7: { #[doc=""]#[doc=""]#[doc=""] {#[doc=""] a::a::a::b as _} },
     err_ap_8: { #[doc=""] {#[doc=""] {}} },
 }
