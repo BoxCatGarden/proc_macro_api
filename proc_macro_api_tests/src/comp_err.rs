d35d71af2b875603635a47ecdb0fe4655973b2e8
boxcatgarden <30717198+BoxCatGarden@users.noreply.github.com>
2025-05-08 07:53:16 +0800

    cargo; test: comp_err, features

--- proc_macro_api_tests/src/comp_err.rs
+++ proc_macro_api_tests/src/comp_err.rs
@@ -4,7 +4,17 @@
 // APIs exported here should have a name starting with `comp_err_`
 // and ending with `_\d+`.
 
-#[cfg(feature = "err_nonexistent_fn")]
+#[cfg(feature = "comp_err_nonexistent_fn")]
 proc_macro_api! {
     #[fn] nonexistent_fn as comp_err_nonexistent_fn_0,
+}
+
+#[cfg(feature = "comp_err_mul_pm_attr")]
+proc_macro_api! {
+    #[fn]#[fn] b as comp_err_mul_pm_attr_0,
+}
+
+#[cfg(feature = "comp_err_mul_pm_attr")]
+proc_macro_api! {
+    #[fn]#[at] c as comp_err_mul_pm_attr_1,
 }
